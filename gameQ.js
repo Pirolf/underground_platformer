b14ac44f3ea3689014a3dfc16000790244c9e999
Yucheng Tu <yuchengtu.uwm@gmail.com>
2014-05-20 20:32:09 -0500

    crouch, and jump up

--- gameQ.js
+++ gameQ.js
@@ -6,6 +6,10 @@
     .touch();                          // Add in touch support (for the UI)
     Q.enableSound();
     Q.setImageSmoothing(false);
+
+    //global vars
+    var isDown = false;
+    var facingRight = true;
     Q.Sprite.extend("Player",{
         init: function(p){
             this._super(p, {
@@ -23,19 +27,51 @@
                 tilew: 64,
                 tileh: 64
             });
+            Q.input.keyboardControls({
+                DOWN: "goDown"
+            });
         },
         step: function(delta){
                 if(this.p.vx > 0){
+                    facingRight = true;
                     if(this.p.landed > 0) {this.play("walk_right");}
                     else {this.play("jump_right");}
                 }else if(this.p.vx < 0){
+                    facingRight = false;
                     if(this.p.landed > 0) { this.play("walk_left");}
                     else{this.play("jump_left");}
-                }else{
-                   // this.play("stand_front");
+                }else if(this.p.vx == 0 && this.p.landed <= 0){
+                    if(facingRight){
+                        this.play("jump_up_facingRight");
+                    }else{
+                        this.play("jump_up_faingLeft");
+                    }
+                }else if(Q.inputs['goDown']){
+                    if(isDown){
+                        isDown = true;
+                        if(facingRight){
+                            this.play("down_right");
+                        }else{
+                            this.play("down_left");
+                        }
+                    }else if(this.p.vx == 0 && this.p.vy == 0 && this.p.landed > 0){
+                        isDown = true;
+                        if(facingRight){
+                           this.play("get_down_right");  
+                        }else{
+                           this.play("get_down_left");  
+                        }                                
+                    }
+                }else if(!Q.inputs["goDown"]){
+                    isDown = false;
+                    if(facingRight){
+                        this.play("stand_right");
+                    }else{
+                        this.play("stand_left");
+                    }
                 }
-            }
-    });
+            },
+     });
 // Load TMX File as a scene
 Q.scene("level1", function(stage){
     Q.stageTMX("underground.tmx", stage);
@@ -45,16 +81,21 @@ Q.scene("level1", function(stage){
 Q.loadTMX("underground.tmx", function(){
     Q.compileSheets("platformer_sprites0.png");
     //Q.stageScene("level1");
-    Q.load(["platformer_sprites0.png", "37_walk.jpg"], function(){
-        
+    Q.load(["platformer_sprites0.png", "37_walk.jpg"], function(){      
         Q.animations("platformer_sprites0", {
             walk_right:  { frames: [34,35,36,37], rate: 1/4, flip: false, loop: true, next: 'stand_right' },
             walk_left:   { frames: [34,35,36,37], rate: 1/4, flip: "x",   loop: true, next: 'stand_left' },
             jump_right:  { frames: [43,44,44,44,44,44,44,45,45,45,45,46,47], rate: 1/10, next: "stand_right", flip: false },
             jump_left:  { frames: [43,44,44,44,44,44,44,45,45,45,45,46,47], rate: 1/10, next: "stand_left", flip: "x" },
+            jump_up_facingRight: {frames:[51], rate:1/2, loop:false, next: "stand_right"},
+            jump_up_faingLeft: {frames:[51], rate:1/2, loop:false, flip:"x", next: "stand_left"},
             stand_front: { frames: [34], flip: false },
             stand_right: { frames: [34], rate: 1/4, flip: false, loop:false},
             stand_left:  { frames: [34], rate: 1/4, flip: "x", loop: false},
+            get_down_right: {frames:[17,18,19,20,21,22], rate: 1/8, loop:false},
+            get_down_left: {frames:[17,18,19,20,21,22], rate: 1/8, flip:"x", loop:false},
+            down_right: {frames: [22], rate: 1, flip:false, loop:true},
+            down_left: {frames: [22], rate: 1, flip:"x", loop:true},
         });
 
     });
